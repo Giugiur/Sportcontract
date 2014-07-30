bc05c1904a8a2b3620e1703fa3c68c72d890bc19
thgoede <t.goede@scoutractor.de>
2014-07-30 17:54:09 +0200

    use  now to check for route

--- app/js/common/controllers/HeaderCtrl.js
+++ app/js/common/controllers/HeaderCtrl.js
@@ -10,9 +10,6 @@ var HeaderCtrl = function($scope,User,Languages,$translate,$rootScope, $location
  	$scope.setDropdown = function(i){
  		$scope.dropdown = i;
  	}
-    $rootScope.onPage = function(url){
-        return $location.path() == url;
-    }
 
     $scope.changeLanguage = function(language){
  		$translate.use(language);
