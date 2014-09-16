7bfcb3fb6fd635e1206057eee9d3c5aa6927e266
thgoede <t.goede@scoutractor.de>
2014-09-16 16:54:06 +0200

    inject translations into settings

--- app/js/countries/controllers/CountriesCtrl.js
+++ app/js/countries/controllers/CountriesCtrl.js
@@ -1,11 +1,6 @@
 var CountriesCtrl = function($scope,$http, Storage, $state, Tabs,ngProgress,countries) {
- 
-
 
   $scope.countries = countries;
-  
-  
-
 
   $scope.goCountry = function(object){
   	$state.go('dashboard.leagues',object);
