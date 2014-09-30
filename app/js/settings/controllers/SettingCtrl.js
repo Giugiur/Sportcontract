7d3b6ff66a4b609ac3fa8e958352c11704cbdbb3
thgoede <t.goede@scoutractor.de>
2014-09-30 11:16:49 +0200

    add list of timezones; all settings will be stored immediatly within the Users object now

--- app/js/settings/controllers/SettingCtrl.js
+++ app/js/settings/controllers/SettingCtrl.js
@@ -5,6 +5,12 @@ var SettingsCtrl = function($scope, $http, User, languages, $translate) {
 
     $scope.languages = languages;
 
+    $scope.timezones;
+
+    $http.get('/timezones.json').success(function(result){
+        $scope.timezones = result;
+    })
+
     $scope.$watch('user.profile.language', function(lang){
         //console.log(lang);
         $translate.use(lang);
